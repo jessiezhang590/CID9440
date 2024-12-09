ef14548fecb650cda25f8191b335b0d070127e2a
jessiezhang590@gmail.com <jessiezhang590@gmail.com>
2024-12-09 05:03:38 +0000

    Models Practice

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -10,6 +10,10 @@ orders as (
 
 ),
 
+orders_fact as (
+    select * from {{ ref('fct_orders') }}
+),
+
 customer_orders as (
 
     select
@@ -17,13 +21,19 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(orders.order_id) as number_of_orders,
+        sum(orders_fact.amount) as lifetime_value
 
     from orders
+
+    left join orders_fact using (customer_id)
+
+    where orders_fact.status = 'success'
 
     group by 1
 
 ),
+
 
 final as (
 
@@ -34,7 +44,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders
+        as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,5 @@
+select 
+    orderid as order_id,
+    amount,
+    status
+from `dbt-tutorial`.stripe.payment
